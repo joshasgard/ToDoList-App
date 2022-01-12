d5b34afd4b8301eb907756ff555e1211c37e9446
joshasgard <idowuodesanmi@gmail.com>
2022-01-12 17:16:03 +0100

    Initial Commit

---
+++ todo/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+
+def signupuser(request):
+
+    return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
+
+# Create your views here.
