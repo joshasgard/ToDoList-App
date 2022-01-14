242d840857b10e80cf3fbc563d979a027e8885d1
joshasgard <idowuodesanmi@gmail.com>
2022-01-14 08:09:31 +0100

    Stackoverflow Linked

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 def signupuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
+    else:
+        # Create a new user
+        if request.POST['password1'] ==request.POST['password2']:
+            user = User.objects.create_user(request.POST['username'],password=request.POST['password1'] )
+            user.save()
+        else:
+            return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error':'Passwords did not match'})
+            #Tell user the passwords didn't match
 
-    return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
-
-# Create your views here.
