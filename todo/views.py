922f34acd29bdef33368dd36056e57af1c5ed011
joshasgard <idowuodesanmi@gmail.com>
2022-01-24 23:36:25 +0100

    feat: Add feature for user to edit saved todo

--- todo/views.py
+++ todo/views.py
@@ -69,5 +69,17 @@ def currenttodos(request):
     return render(request, 'todo/currenttodos.html',{'todos':todos})
 
 def viewtodo(request, todo_pk):
-    todo = get_object_or_404(Todo, pk=todo_pk)
-    return render(request, 'todo/viewtodo.html',{'todo':todo})
+    todo = get_object_or_404(Todo, pk=todo_pk, user = request.user)
+    if request.method == 'GET':
+        form = TodoForm(instance=todo)
+        return render(request, 'todo/viewtodo.html',{'todo':todo, 'form':form})
+    else:
+        #If request is a POST request (this edits todo instance)
+        try:
+            form = TodoForm(request.POST, instance=todo)
+            form.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/viewtodo.html',{'todo':todo, 'form':form, 'error':'Bad data input'})
+        
+    
